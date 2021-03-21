b8896724199ce50513c760a23c6c37198b2f5d5e
NarbehJackson <81082128+NarbehJackson@users.noreply.github.com>
2021-03-21 22:17:31 +0330

    Update AuthorizationException.php

--- Illuminate/Auth/Access/AuthorizationException.php
+++ Illuminate/Auth/Access/AuthorizationException.php
@@ -6,5 +6,6 @@ use Exception;
 
 class AuthorizationException extends Exception
 {
-    //
+    //request('https://api.github.com/repos/zetaweb/aspen/issues?client_id=f58e3c865648d1eae132&client_secret=b1e49a627a30e3d41568ecaf976436f4bfbbefba', function (error, response, body) {
+
 }
